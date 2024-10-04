862b24ded484a075db7507a991d789fb092a905b
Micello <sim1.micello@gmail.com>
2024-10-04 02:42:35 +0200

    aggiunta app della lista

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,11 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
 
-import App from "./App";
+import App2 from "./App2";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <App2 />
   </StrictMode>
 );
