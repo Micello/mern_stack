7d8992cf7ed89213892e3d5834d8aed5657bfd4e
Micello <sim1.micello@gmail.com>
2024-10-06 03:36:03 +0200

    aggiunta mano arcuata

--- frontend/src/Briscola.js
+++ frontend/src/Briscola.js
@@ -1,22 +1,42 @@
-function Card({id, rank, suit}){
+
+
+function Card({id, rank, suit, angle, height}){
     return(
         <>
-        {rank ? <div className="Card">{id + rank + suit}</div>: <div className="Card">Back</div>} 
-        </>
+            {rank ? (
+            <div
+            style={{ transform: `rotate(${angle}rad) translateY(${height*10}px) translateX(${angle*-400}px)` }} 
+            className={`relative  focus:ring-violet-300 active:bg-violet-700 bg-[lightgrey] hover:bg-gray-50 h-48 w-32 box-border m-2 p-4 border-2 border-solid border-[black]`}
+    
+            >
+                {id + rank + suit + '                ' + angle + height}
+            </div>
+            ) : (
+            <div className="bg-[lightgrey] w-24 h-40 m-2 p-4 border-2 border-solid border-[black]">
+                Back
+            </div>
+            )}
+      </>
     )
-};
-
-function Hand({playerHand}){
-    return(
-        <ul>
-            {playerHand.map((card) =>(
-                <li key={card.id}>
-                    <Card id={card.id} rank={card.rank} suit={card.suit} />
-                </li>
-            ))}
-        </ul>
-    );
 }
+
+function Hand({ playerHand }) {
+    return (
+      <ul className="flex justify-center items-center  ">
+        {playerHand.map((card,index) => {
+            const angle = 0.06*(card.id - ((playerHand.length+1)/2))
+            const height = 0.5*(card.id - (playerHand.length+1)/2)*(card.id-(playerHand.length+1)/2)
+            console.log(height)
+            return (
+            <li key={card.id}>
+              <Card id={card.id} rank={card.rank} suit={card.suit} angle={angle} height={height} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+  
 
 function Board({}){
     return(
@@ -47,7 +67,12 @@ export default function Briscola(){
 const playerHand = [
     { id: 1, rank: "A", suit: "Spades" },
     { id: 2, rank: "10", suit: "Hearts" },
-    { id: 3, rank: "K", suit: "Clubs" }
+    { id: 3, rank: "K", suit: "Clubs" },
+    { id: 4, rank: "K", suit: "Clubs" },
+    { id: 5, rank: "K", suit: "Clubs" },
+    { id: 6, rank: "K", suit: "Clubs" }
+
+    
 ];
 
 const enemyHand = [
